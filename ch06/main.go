4978be626e3c38277cdc089cdeb3936a5bff3130
Paisit Jarunnamsiri <paisitj@Paisits-MacBook-Pro-14.local>
2024-04-25 17:36:47 -0700

    ch06

---
+++ ch06/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/paisit04/building-modern-cli/ch06/examples"
+
+func main() {
+	// examples.CreateCommandUsingStruct()
+	// examples.CreateCommandUsingCommandFunction()
+	// examples.RunMethod()
+	// examples.StartMethod()
+	// examples.OutputMethod()
+	// examples.CombinedOutputMethod()
+	examples.Limit()
+}
